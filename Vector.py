b5babebe24559153055b3c49a619212a47239b4f
vjcalling <vibhor.jain@outlook.com>
2018-09-08 10:58:18 +0530

    adding vector class

---
+++ Vector.py
@@ -0,0 +1,21 @@
+
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple([Decimal(c) for c in coordinates])
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+    def __str__(self):
+        return 'Vector: {}'.format([round(coord, 3)
+                                    for coord in self.coordinates])
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
